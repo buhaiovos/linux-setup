913d3152628d047c5a87fffdb6900e12ccd9efe1
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-03-21 22:42:30 +0200

    Updates to set-up script

--- set-up.sh
+++ set-up.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
+echo '*********************************'
 echo 'Installing the latest git version'
+echo '*********************************'
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get install git
 
+echo '*****************'
 echo 'Installing SDKMAN'
+echo '*****************'
 sudo curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
 
+echo '*****************'
 echo 'Installing Docker'
+echo '*****************'
 sudo apt install docker.io
 echo 'Enabling Docker'
 sudo systemctl start docker
 sudo systemctl enable docker
 
+echo '*********************************'
 echo 'Installing Intellij Idea Ultimate'
+echo '*********************************'
 sudo snap install intellij-idea-ultimate --classic
 
+echo '**************'
 echo 'Installing ZSH'
+echo '**************'
 sudo apt install zsh
 zsh --version
 chsh -s $(which zsh)
-gnome-terminal --tab --command="sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
